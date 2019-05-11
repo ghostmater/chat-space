1d2a7594f6333a32cb236386fdc194451443cf51
ghostmater <gostmater@gmail.com>
2019-05-11 12:58:46 +0900

    asynchronous message send

---
+++ app/assets/javascripts/channels/message.js
@@ -0,0 +1,49 @@
+$(function(){
+
+  function buildHTML(message){
+    var image = message.image.url ? message.image.url : "";
+    var html = `<div class="message" data-id=${message.id}>
+                  <div class="upper-info">
+                    <p class="upper-info__user">
+                      ${message.user_name}
+                    </p>
+                    <p class="upper-info__date">
+                      ${message.created_at}
+                    </p>
+                  </div>`;
+    var content = message.content ? `<p class="message__text">
+                                       ${message.content}
+                                     </p>` : "";
+
+    return html + content + `<img class="image" src=${image}></div>`;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    console.log($('#new_message')[0]);
+    console.log('debug');
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#new_message')[0].reset(); // 入力欄クリア
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); // スクロール
+    })
+    .fail(function(){
+      alert('error');
+    })
+    .always(function() { // ボタンをenableにする処理
+      $('.send__button').removeAttr('disabled');
+    });
+  });
+})
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.(@message, :content, :image)
+json.user_name @message.user.name
+json.id @message.id
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M:%S")
