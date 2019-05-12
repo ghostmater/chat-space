cd96cbfe9e0965ac258d10ae6f1ba6aa7ab79e90
ghostmater <gostmater@gmail.com>
2019-05-12 14:28:31 +0900

    implement incremental search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{params[:user_name]}%").limit(20)
+    respond_to do |format|
+      format.json # view/index.json.builderでデータをjson形式にする
+    end
+  end
+
   def edit
   end
 
